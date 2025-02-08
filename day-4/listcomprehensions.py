9d22034843eeac8caddf4527cf4bf88dedf5c1e0
Kush Doshi <kushdoshi@vebuin.com>
2025-02-08 15:20:02 +0530

    list comprehensions example file

---
+++ day-4/listcomprehensions.py
@@ -0,0 +1,9 @@
+squares = []
+
+# for i in range(10):
+#     squares.append(i * i)
+
+# List comprehension to generate a list of squares of numbers from 0 to 9
+squares = [i * i for i in range(10)]  
+
+print(squares)
