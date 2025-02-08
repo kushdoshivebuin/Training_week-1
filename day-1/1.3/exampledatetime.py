702fc41714a376c5c813fbaca91b686910668785
Kush Doshi <kushdoshi@vebuin.com>
2025-02-08 14:55:01 +0530

    exampledatetime.py committed

---
+++ day-1/1.3/exampledatetime.py
@@ -0,0 +1,9 @@
+import datetime
+
+# Get the current date
+current_date = datetime.date.today()
+print("Current date:", current_date)
+
+# Get the current date and time
+current_time = datetime.datetime.now()
+print("Current date and time:", current_time)
