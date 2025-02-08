eebffa20a9fc912d05eb707ddc321b80f39388e8
Kush Doshi <kushdoshi@vebuin.com>
2025-02-08 15:17:25 +0530

    function docstrings file

---
+++ day-4/function_docstrings.py
@@ -0,0 +1,15 @@
+def add(a:int , b:int)-> int :
+    """
+    Adds two numbers together.
+
+    Parameters :
+    a (int) : The first number.
+    b (int) : The second number.
+
+    Returns :
+    int : The sum of the two numbers.
+    """
+
+    return a + b
+
+print(add(6,7))
