8328c10bc43b7e77d9c7d0050fed69bdca201dfc
Kush Doshi <kushdoshi@vebuin.com>
2025-02-08 15:15:28 +0530

    docstrings for functions

---
+++ day-4/docstrings.py
@@ -0,0 +1,11 @@
+def square(n)-> None:
+    """
+    params:
+        n: int number
+    
+    Takes n as an input and prints the square of the number.
+    """
+    print(n*n)
+
+square(5)
+help(square)
