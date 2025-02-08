fb771329bcfc815b7220324a0c4a3ca1bb5660db
Kush Doshi <kushdoshi@vebuin.com>
2025-02-08 15:01:29 +0530

    package structure program

---
+++ day-1/1.5/main.py
@@ -0,0 +1,12 @@
+# Importing specific modules and sub-modules from the 'my_package' package
+from my_package import module1, module2, sub_package
+from my_package.sub_package import sub_module
+
+# Call a function from 'module1'
+module1.module1_function()
+
+# Call a function from 'module2'
+module2.module2_function()
+
+# Call a function from 'sub_module' inside 'sub_package'
+sub_module.submodule_function()
