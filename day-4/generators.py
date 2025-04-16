cdba9c695007a2d9b3edbe758f09489fad46e71c
Kush Doshi <kushdoshi@vebuin.com>
2025-04-16 21:33:05 +0530

    Update generators.py

--- day-4/generators.py
+++ day-4/generators.py
@@ -2,9 +2,8 @@ import sys
 
 my_list = [i for i in range(10000)]
 print(sum(my_list))  
-print(sys.getsizeof(my_list), "bytes")  # Memory size of the list
+print(sys.getsizeof(my_list), "bytes")  
 
-# Generator expression to create a generator containing numbers from 0 to 9999
-my_gen = (i for i in range(10000))  # Generators are used to save memory, values are computed lazily
+my_gen = (i for i in range(10000)) 
 print(sum(my_gen))  
-print(sys.getsizeof(my_gen), "bytes")  # Memory size of the generator
+print(sys.getsizeof(my_gen), "bytes")
