e93199f47040e89fe515085274cc957071fde736
Kush Doshi <kushdoshi@vebuin.com>
2025-04-16 21:33:43 +0530

    Update sorted.py

--- day-4/sorted.py
+++ day-4/sorted.py
@@ -11,8 +11,6 @@ data = [{"name": "Max", "age": 6},
         {"name": "Lisa", "age": 20},
         {"name": "Ben", "age": 9}]
 
-# Sorting the list of dictionaries based on the 'age' key using a lambda function as the key argument
-# The lambda function extracts the 'age' value from each dictionary to use it as the sorting criterion.
 sorted_data = sorted(data, key=lambda x: x["age"])
 
 print(sorted_data)
