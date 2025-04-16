f971cfe65583b726c22928a2b91e2af343f81804
Kush Doshi <kushdoshi@vebuin.com>
2025-04-16 21:27:07 +0530

    Update osexample.py

--- day-1/1.3/osexample.py
+++ day-1/1.3/osexample.py
@@ -1,9 +1,7 @@
 import os
 
-# Get the current working directory
 current_dir = os.getcwd()
 print("Current working directory:", current_dir)
 
-# Create a new directory named "1.4" in the current directory
 os.mkdir("1.4")
 print("Directory '1.4' has been created.")
