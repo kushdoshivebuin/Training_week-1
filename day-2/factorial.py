64d7d7a1b757b87b7e2ce17d8050297117370f4a
Kush Doshi <kushdoshi@vebuin.com>
2025-04-16 21:30:59 +0530

    Update factorial.py

--- day-2/factorial.py
+++ day-2/factorial.py
@@ -17,16 +17,13 @@ class Factorial:
         int: The factorial of the number.
         """
         result = 1
-        # Base case: Factorial of 0 is 1
+       
         if num == 0:
             return 1
         
-        # Recursive case: Multiply current number by the factorial of (num-1)
         return self.calc_fact(num - 1) * num
 
-# Input number for which the factorial is to be calculated
 n = int(input("Enter the number for factorial: "))
 
-# Create an instance of the Factorial class and compute the factorial
 f1 = Factorial()
-print(f1.calc_fact(n))  # Output the factorial of the number
+print(f1.calc_fact(n)) 
